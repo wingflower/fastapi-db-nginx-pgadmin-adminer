dbf55bd4ea07a274b9e787575a0d458d82b10954
Dongho Gwak <wing.flower.k@gmail.com>
2024-03-21 14:07:35 +0900

    Router 추가 및 서버 시작/종료 이벤트 적용

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
-from fastapi import FastAPI
 import uvicorn
 
+from fastapi import FastAPI
+
+from app.routes import index
+
+
 app = FastAPI()
+app.include_router(index.router)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event("startup")
+async def on_app_start():
+    print("-----[Welcome]-----")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.on_event("shutdown")
+async def on_app_stop():
+    print("-----[Bye]-----")
 
 
 if __name__ == "__main__":
